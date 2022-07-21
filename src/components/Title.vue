3b0f5859e6854d47fa100e9452f40906531f09a1
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-21 16:45:44 +0200

    complete project

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>La mia prima app con Vue CLI!</div>
+</template>
+
+<script>
+export default {
+  name: "MyTitle",
+};
+</script>
+
+<style scoped lang="scss">
+title {
+  color: orange;
+}
+</style>
